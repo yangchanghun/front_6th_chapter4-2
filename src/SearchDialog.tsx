38f04d3b1ed7a89f461a7171765ad328c0699dfb
yangchanghun <gory4848@naver.com>
2025-09-11 11:44:52 +0900

    병렬처리

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -107,11 +107,17 @@ const fetchLiberalArts = checkCached(() =>
 //   axios.get<Lecture[]>("/schedules-liberal-arts.json");
 
 // TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-const fetchAllLectures = async () =>
-  await Promise.all([
+const fetchAllLectures = async () => {
+  const [majorsRes, liberalRes] = await Promise.all([
     (console.log("API Call 1", performance.now()), fetchMajors()),
     (console.log("API Call 2", performance.now()), fetchLiberalArts()),
+    (console.log("API Call 3", performance.now()), fetchMajors()),
+    (console.log("API Call 4", performance.now()), fetchLiberalArts()),
+    (console.log("API Call 5", performance.now()), fetchMajors()),
+    (console.log("API Call 6", performance.now()), fetchLiberalArts()),
   ]);
+  return [...majorsRes.data, ...liberalRes.data];
+};
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
@@ -207,7 +213,7 @@ const SearchDialog = ({ searchInfo, onClose }: Props) => {
       const end = performance.now();
       console.log("모든 API 호출 완료 ", end);
       console.log("API 호출에 걸린 시간(ms): ", end - start);
-      setLectures(results.flatMap((result) => result.data));
+      setLectures(results);
     });
   }, []);
 
